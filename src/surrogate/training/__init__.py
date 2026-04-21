edca58c775bfafddb2b32d7f79f1601d4153ce1b
LeoLeng <2321063338@qq.com>
2026-04-21 16:13:57 +0800

    feat(surrogate): add residual embedding MLP classifier

--- src/surrogate/training/__init__.py
+++ src/surrogate/training/__init__.py
@@ -2,6 +2,7 @@ from .catboost_baseline import CatBoostBaselineConfig, run_catboost_baseline
 from .lightgbm_baseline import LightGBMBaselineConfig, run_lightgbm_baseline
 from .mlp_classifier import MLPClassifierConfig, run_mlp_classifier
 from .mlp_embedding_classifier import MLPEmbeddingConfig, run_mlp_embedding
+from .mlp_residual_classifier import MLPResidualConfig, run_mlp_residual
 
 __all__ = [
     "LightGBMBaselineConfig",
@@ -12,4 +13,6 @@ __all__ = [
     "run_mlp_embedding",
     "MLPClassifierConfig",
     "run_mlp_classifier",
+    "MLPResidualConfig",
+    "run_mlp_residual",
 ]
