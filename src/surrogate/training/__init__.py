fc1368fd52176b5edceeac1de62dcc047282631b
LeoLeng <2321063338@qq.com>
2026-04-21 16:00:12 +0800

    feat(surrogate): add embedding MLP classifier

--- src/surrogate/training/__init__.py
+++ src/surrogate/training/__init__.py
@@ -1,12 +1,15 @@
 from .catboost_baseline import CatBoostBaselineConfig, run_catboost_baseline
 from .lightgbm_baseline import LightGBMBaselineConfig, run_lightgbm_baseline
 from .mlp_classifier import MLPClassifierConfig, run_mlp_classifier
+from .mlp_embedding_classifier import MLPEmbeddingConfig, run_mlp_embedding
 
 __all__ = [
     "LightGBMBaselineConfig",
     "run_lightgbm_baseline",
     "CatBoostBaselineConfig",
     "run_catboost_baseline",
+    "MLPEmbeddingConfig",
+    "run_mlp_embedding",
     "MLPClassifierConfig",
     "run_mlp_classifier",
 ]
