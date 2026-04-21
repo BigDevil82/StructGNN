ddffec5692a7e06294d834c87b30bb256766058b
LeoLeng <2321063338@qq.com>
2026-04-21 16:31:01 +0800

    删除无用的模型、将surrogate相关脚本移入文件夹

--- src/surrogate/training/__init__.py
+++ src/surrogate/training/__init__.py
@@ -1,9 +1,7 @@
 from .catboost_baseline import CatBoostBaselineConfig, run_catboost_baseline
 from .lightgbm_baseline import LightGBMBaselineConfig, run_lightgbm_baseline
-from .mlp_classifier import MLPClassifierConfig, run_mlp_classifier
 from .mlp_embedding_classifier import MLPEmbeddingConfig, run_mlp_embedding
 from .mlp_embedding_kfold import MLPEmbeddingKFoldConfig, run_mlp_embedding_kfold
-from .mlp_residual_classifier import MLPResidualConfig, run_mlp_residual
 
 __all__ = [
     "LightGBMBaselineConfig",
@@ -14,8 +12,4 @@ __all__ = [
     "run_mlp_embedding",
     "MLPEmbeddingKFoldConfig",
     "run_mlp_embedding_kfold",
-    "MLPClassifierConfig",
-    "run_mlp_classifier",
-    "MLPResidualConfig",
-    "run_mlp_residual",
 ]
